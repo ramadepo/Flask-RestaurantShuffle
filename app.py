c316ca438d97cecde62d98058695512c60dd0037
Rama <iammastercloud@gmail.com>
2019-06-10 23:02:51 +0800

    bootstrap css js jquery

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/search/')
-def search():
-    return 'Search ' + request.args.get('s1') + ' ' + request.args.get('s2')
+# @app.route('/search/')
+# def search():
+#     return 'Search ' + request.args.get('s1') + ' ' + request.args.get('s2')
 
 
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    elif request.method == 'POST':
-        return f"Your username is {request.form.get('username')} \t\t Your password is {request.form.get('password')}"
+# @app.route('/login/', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'GET':
+#         return render_template('login.html')
+#     elif request.method == 'POST':
+#         return f"Your username is {request.form.get('username')} \t\t Your password is {request.form.get('password')}"
 
 
 if __name__ == '__main__':
