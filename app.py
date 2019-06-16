19433e7e92e8311688243a8b49cdea3c27c725bd
Rama <iammastercloud@gmail.com>
2019-06-16 12:06:45 +0800

    add UC in model and create region.html, shop.html
    TODO: use flask restful api package

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from exts import db
 import config
-from models import *
+from models import Region, Shop
 
 
 app = Flask(__name__)
@@ -12,6 +12,49 @@ db.init_app(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/region', methods=['POST'])
+def create_region():
+    name = request.form.get('name')
+
+    region = Region(name=name)
+    db.session.add(region)
+    db.session.commit()
+
+    return redirect(url_for('get_regions'))
+
+
+@app.route('/region')
+def get_regions():
+    return render_template('region.html')
+
+
+@app.route('/region/<id>')
+def get_region(id):
+    pass
+
+
+@app.route('/shop', methods=['POST'])
+def create_shop():
+    name = request.form.get('name')
+    region_id = request.form.get('region_id')
+
+    shop = Shop(name=name, region_id=region_id)
+    db.session.add(shop)
+    db.session.commit()
+
+    return redirect(url_for('get_shops'))
+
+
+@app.route('/shop')
+def get_shops():
+    return render_template('shop.html')
+
+
+@app.route('/shop/<id>')
+def get_shop(id):
+    pass
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,12 +3,18 @@ from exts import db
 
 class Region(db.Model):
     __tablename__ = 'region'
+    __table_args__ = (
+        db.UniqueConstraint('name', name='UC_Region'),
+    )
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
 
 
 class Shop(db.Model):
     __tablename__ = 'shop'
+    __table_args__ = (
+        db.UniqueConstraint('name', 'region_id', name='UC_Shop'),
+    )
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     region_id = db.Column(db.Integer, db.ForeignKey(
