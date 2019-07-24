58a2d690b0c7720f15ec0c2c054830977bef4867
Rama <iammastercloud@gmail.com>
2019-07-24 19:19:02 +0800

    fix auto python manage bug and complete docker-compose.yml

--- restaurantshuffle/init.sh
+++ restaurantshuffle/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ ! -d migrations ]; then
-    python manage.py db init && python manage.py db migrate && python manage.py db upgrade
+    python manage.py db init && \
+    sleep 10 && \
+    python manage.py db migrate && \
+    sleep 5 && \
+    python manage.py db upgrade
 fi
 
 uwsgi uwsgi/restaurantshuffle.ini
